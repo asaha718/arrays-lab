afa739865a69eec8fecd36b517be7d8a5eb29fa4
Anug Saha <anugsaha@gmail.com>
2022-06-27 12:11:49 -0400

    add students to array and print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        Scanner scanner = new Scanner(System.in);
+        String[] allStudents = new String[10];
+        int count = 0;
+
+        while (count < 10) {
+            System.out.println("enter student name: ");
+            String student = scanner.nextLine();
+            allStudents[count] = student;
+            count++;
+        }
+        scanner.close();
+        
+        System.out.println("You have entered the following names:");
+
+        for (String student : allStudents) {
+            System.out.println(student);
+        }
+
     }
 }
